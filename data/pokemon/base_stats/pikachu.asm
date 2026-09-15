11f4303cfefc99d41fbf523179fb7cbae92891a5
Jennifer Tesolin <3802757+jennifert@users.noreply.github.com>
2026-09-15 19:18:40 -0400

    Add initial partner Pikachu enhancements

--- data/pokemon/base_stats/pikachu.asm
+++ data/pokemon/base_stats/pikachu.asm
@@ -1,6 +1,6 @@
 	db DEX_PIKACHU ; pokedex id
 
-	db  35,  55,  30,  90,  50
+	db  60,  90,  55, 100,  90
 	;   hp  atk  def  spd  spc
 
 	db ELECTRIC, ELECTRIC ; type
@@ -18,7 +18,7 @@
 	     DOUBLE_EDGE,  PAY_DAY,      SUBMISSION,   SEISMIC_TOSS, RAGE,         \
 	     THUNDERBOLT,  THUNDER,      MIMIC,        DOUBLE_TEAM,  REFLECT,      \
 	     BIDE,         SWIFT,        SKULL_BASH,   REST,         THUNDER_WAVE, \
-	     SUBSTITUTE,   FLASH
+	     SUBSTITUTE,   FLASH,        SURF
 	; end
 
 	db 0 ; padding
